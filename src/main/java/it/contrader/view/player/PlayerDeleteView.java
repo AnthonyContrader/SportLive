d73b424735fa55d30546af857e47d2ebb0afc1fc
antimogit <antimowork@gmail.com>
2019-06-20 21:35:40 +0200

    aggiornamento playerdeleteview

--- src/main/java/it/contrader/view/player/PlayerDeleteView.java
+++ src/main/java/it/contrader/view/player/PlayerDeleteView.java
@@ -1,5 +1,39 @@
 package it.contrader.view.player;
 
-public class PlayerDeleteView {
+import it.contrader.controller.Request;
+import it.contrader.main.MainDispatcher;
+import it.contrader.view.AbstractView;
 
+public class PlayerDeleteView extends AbstractView {
+	private Request request;
+	
+	private int id;
+	private int idcoach;
+	private final String mode = "DELETE";
+	
+	public PlayerDeleteView() {
+	}
+	
+	public void showResults(Request request) {
+		if (request!=null) {
+			System.out.println("Cancellazione andata a buon fine.\n");
+			MainDispatcher.getInstance().callView("Player", null);
+ }
 }
+	public void showOptions() {
+		System.out.println("Inserisci id dell'utente:");
+		id = Integer.parseInt(getInput());
+	}
+
+	
+		// TODO Auto-generated method stub
+
+		public void submit() {
+			request = new Request();
+			request.put("id", id);
+			request.put("idcoach", idcoach);
+			request.put("mode", mode);
+			MainDispatcher.getInstance().callAction("Player", "doControl", request);
+		}		
+	}
+	
