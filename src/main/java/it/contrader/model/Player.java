42e74d09ba05858e152559e78caad71b86b89fa1
unknown <sasy93t@hotmail.it>
2019-06-23 23:24:59 +0200

    Aggiornato Player Model

--- src/main/java/it/contrader/model/Player.java
+++ src/main/java/it/contrader/model/Player.java
@@ -12,15 +12,31 @@ public class Player {
 	
 	private String nickname;
 	
+	private int age;
+	
+	private double weight;
+	
+	private int height;
+	
+	private int gp;
+	
+	private int score;
+	
+	
+	
 	public Player () {
 		
 	}
-	
-	public Player (int idcoach,  String playertype, String password, String nickname) {
+	 Player (int idcoach, String playertype, String password, String nickname, int age, int height, double weight, int gp, int score) {
 		this.idcoach = idcoach;
 		this.password = password;
 		this.playertype = playertype;
 		this.nickname = nickname;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		this.gp = gp;
+		this.score = score;
 	}
 
 	public int getId() {
@@ -62,10 +78,49 @@ public class Player {
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
 	}
+	
+	public int getAge() {
+		return age;
+	}
 
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	public double getWeight() {
+		return weight;
+	}
+	
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+	public int getGp() {
+		return gp;
+	}
+	
+	public void setGp(int height) {
+		this.gp = gp;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void set(int score) {
+		this.score = score;
+	}
 	@Override
 	public String toString() {
-		return  id + "\t"  + idcoach +"\t\t" + password + "\t\t" +  playertype + "\t\t" + nickname;
+		return  id + "\t"  + idcoach +"\t\t" + password + "\t\t" +  playertype + "\t\t" + nickname + "\t\t" + age + "\t\t" + height + "\t\t" + weight + "\t\t" + gp + "\t\t" + score;
 	}
 	
 	
@@ -83,20 +138,21 @@ public class Player {
 			return false;
 		if(idcoach != other.idcoach)
 			return false;
-		if (password == null) {
-			if (other.password != null)
-				return false;
-		} else if (!password.equals(other.password))
+		if (password != other.password)
 			return false;
-		if (playertype == null) {
-			if (other.playertype != null)
-				return false;
-		} else if (!playertype.equals(other.playertype))
+		if (playertype != other.playertype)
 			return false;
-		if (nickname == null) {
-			if (other.nickname != null)
-				return false;
-		} else if (!nickname.equals(other.nickname))
+		if (nickname != other.nickname)
+			return false;
+		if (age != other.age)
+			return false;
+		if (height != other.height)
+			return false;
+		if (weight != other.weight)
+			return false;
+		if (gp != other.gp)
+			return false;
+		if (score != other.score)
 			return false;
 		return true;
 	}
