fdc6e276909772cfdd68292b2e123d11a6b45783
unknown <sasy93t@hotmail.it>
2019-06-27 14:35:11 +0200

    Aggiunto Device e Players Servlets

---
+++ src/main/java/it/contrader/servlets/DeviceServlet.java
@@ -0,0 +1,97 @@
+package it.contrader.servlets;
+
+import java.util.List;
+
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import it.contrader.dto.DeviceDTO;
+import it.contrader.service.Service;
+import it.contrader.service.DeviceService;
+
+/**
+ * Servlet implementation class DeviceServlet
+ */
+public class DeviceServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeviceServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	public void updateList(HttpServletRequest request) {
+		Service<DeviceDTO> service = new DeviceService();
+		List<DeviceDTO>listDTO = service.getAll();
+		request.setAttribute("list", listDTO);
+	}
+
+	@Override
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Service<DeviceDTO> service = new DeviceService();
+		String mode = request.getParameter("mode");
+		DeviceDTO dto;
+		int idplayer, heartbeat, pressure, breath;
+		boolean ans;
+
+		switch (mode.toUpperCase()) {
+
+		case "DEVICELIST":
+			updateList(request);
+			getServletContext().getRequestDispatcher("/device/devicemanager.jsp").forward(request, response);
+			break;
+
+		case "READ":
+			idplayer = Integer.parseInt(request.getParameter("idplayer"));
+			dto = service.read(idplayer);
+			request.setAttribute("dto", dto);
+			
+			if (request.getParameter("update") == null) {
+				 getServletContext().getRequestDispatcher("/device/readdevice.jsp").forward(request, response);
+				
+			}
+			
+			else getServletContext().getRequestDispatcher("/device/updatedevice.jsp").forward(request, response);
+			
+			break;
+
+		case "INSERT":
+			
+			heartbeat = Integer.parseInt(request.getParameter("heartbeat"));
+			pressure = Integer.parseInt(request.getParameter("pressure"));
+			breath = Integer.parseInt(request.getParameter("breath"));
+			dto = new DeviceDTO (heartbeat, pressure, breath);
+			ans = service.insert(dto);
+			request.setAttribute("ans", ans);
+			updateList(request);
+			getServletContext().getRequestDispatcher("/device/devicemanager.jsp").forward(request, response);
+			break;
+			
+		case "UPDATE":
+			heartbeat = Integer.parseInt(request.getParameter("heartbeat"));
+			pressure = Integer.parseInt(request.getParameter("pressure"));
+			breath = Integer.parseInt(request.getParameter("breath"));
+			idplayer = Integer.parseInt(request.getParameter("idplayer"));
+			dto = new DeviceDTO (idplayer, heartbeat, pressure, breath);
+			ans = service.update(dto);
+			updateList(request);
+			getServletContext().getRequestDispatcher("/device/devicemanager.jsp").forward(request, response);
+			break;
+
+		case "DELETE":
+			idplayer = Integer.parseInt(request.getParameter("idplayer"));
+			ans = service.delete(idplayer);
+			request.setAttribute("ans", ans);
+			updateList(request); 
+			getServletContext().getRequestDispatcher("/device/devicemanager.jsp").forward(request, response);
+			break;
+		}
+	}
+}
---
+++ src/main/java/it/contrader/servlets/PlayersServlet.java
@@ -0,0 +1,69 @@
+package it.contrader.servlets;
+
+import java.util.List;
+
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import it.contrader.dto.PlayersDTO;
+import it.contrader.service.Service;
+import it.contrader.service.PlayersService;
+
+/**
+ * Servlet implementation class PlayerServlet
+ */
+public class PlayersServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PlayersServlet() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void updateList(HttpServletRequest request) {
+		Service<PlayersDTO> service = new PlayersService();
+		List<PlayersDTO>listDTO = service.getAll();
+		request.setAttribute("list", listDTO);
+    }
+
+    
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Service<PlayersDTO> service = new PlayersService();
+		String mode = request.getParameter("mode");
+		PlayersDTO dto;
+		int id;
+		boolean ans;
+
+		switch (mode.toUpperCase()) {
+
+		case "PLAYERSLIST":
+			updateList(request);
+			getServletContext().getRequestDispatcher("/players/playersmanager.jsp").forward(request, response);
+			break;
+
+		case "READ":
+			id = Integer.parseInt(request.getParameter("id"));
+			dto = service.read(id);
+			request.setAttribute("dto", dto);
+			
+			if (request.getParameter("update") == null) {
+				 getServletContext().getRequestDispatcher("/players/readparameters.jsp").forward(request, response);
+				
+			}
+			
+			else getServletContext().getRequestDispatcher("/players/updateparameters.jsp").forward(request, response);
+			
+			break;
+		}
+	}
+}
+
+
+    
+
