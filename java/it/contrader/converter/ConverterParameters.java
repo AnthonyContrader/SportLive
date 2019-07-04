385db93c3027ce3f6830b44f84d61ee504afbd0a
edoardo meli <edoxmeli@gmail.com>
2019-07-04 14:50:03 +0200

    pippo

---
+++ java/it/contrader/converter/ConverterParameters.java
@@ -0,0 +1,64 @@
+package it.contrader.converter;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.contrader.dto.ParametersDTO;
+import it.contrader.model.Parameters;
+
+public class ConverterParameters {
+
+	public static ParametersDTO toDTO(Parameters parameters) {
+		ParametersDTO parametersDTO = null;
+		if (parameters != null) {
+			parametersDTO = new ParametersDTO();
+			parametersDTO.setIdplayer(parameters.getIdplayer());
+			parametersDTO.setData(parameters.getData());
+			parametersDTO.setAge(parameters.getAge());
+			parametersDTO.setHeight(parameters.getHeight());
+			parametersDTO.setWeight(parameters.getWeight());
+			parametersDTO.setGol(parameters.getGol());
+			parametersDTO.setGp(parameters.getGp());
+			parametersDTO.setMp(parameters.getMp());
+		}
+		return parametersDTO;
+	}
+
+	public static Parameters toEntity(ParametersDTO parametersDTO) {
+		Parameters parameters = null;
+		if (parametersDTO != null) {
+			parameters = new Parameters();
+			parameters.setIdplayer(parametersDTO.getIdplayer());
+			parameters.setData(parametersDTO.getData());
+			parameters.setAge(parametersDTO.getAge());
+			parameters.setHeight(parametersDTO.getHeight());
+			parameters.setWeight(parametersDTO.getWeight());
+			parameters.setGol(parametersDTO.getGol());
+			parameters.setGp(parametersDTO.getGp());
+			parameters.setMp(parametersDTO.getMp());
+		}
+		return parameters;
+	}
+
+	public static List<ParametersDTO> toListDTO(List<Parameters> list) {
+		List<ParametersDTO> listParametersDTO = new ArrayList<>();
+		if (!list.isEmpty()) {
+			for (Parameters parameters : list) {
+				listParametersDTO.add(ConverterParameters.toDTO(parameters));
+			}
+		}
+		return listParametersDTO;
+	}
+
+	public static List<Parameters> toListEntity(List<ParametersDTO> listParametersDTO) {
+		List<Parameters> list = new ArrayList<>();
+		if (!listParametersDTO.isEmpty()) {
+			for (ParametersDTO parametersDTO : listParametersDTO) {
+				list.add(ConverterParameters.toEntity(parametersDTO));
+			}
+		}
+		return list;
+	}
+}
+
---
+++ src/main/webapp/homeplayer.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+<h1>Welcome player</h1>
+
+<h2>non puoi fare un c***o :)</h2>
+
+<nav class='navbar navbar-inverse'>
+     <div class='container-fluid'>
+         <ul class='nav navbar-nav navbar-inverse navbar-custom'>
+            <li><a href="/Home/logout/">Vattene!</a></li>
+         </ul>
+    </div>
+</nav>
+
+</body>
+</html>
